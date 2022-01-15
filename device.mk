7d7d0dbeff555753e0f732f8e917798adf29a815
Nikhil <renjian503@gmail.com>
2022-01-15 21:59:52 +0530

    hanoip

--- device.mk
+++ device.mk
@@ -6,10 +6,20 @@
 #
 
 LOCAL_PATH := device/motorola/hanoip
+
+# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
 # A/B
 AB_OTA_PARTITIONS += \
     boot \
+    dtbo \
+    odm \
+    product \
     system \
+    system_ext \
+    vbmeta \
+    vbmeta_system \
     vendor
 
 AB_OTA_POSTINSTALL_CONFIG += \
@@ -17,6 +27,9 @@ AB_OTA_POSTINSTALL_CONFIG += \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 30
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
@@ -38,3 +51,28 @@ PRODUCT_PACKAGES += \
     update_engine \
     update_verifier \
     update_engine_sideload
+
+# API
+PRODUCT_SHIPPING_API_LEVEL := 30
+
+# tell update_engine to not change dynamic partition table during updates
+# needed since our qti_dynamic_partitions does not include
+# vendor and odm and we also dont want to AB update them
+TARGET_ENFORCE_AB_OTA_PARTITION_LIST := true
+
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
+# qcom decryption
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
+
+# Apex libraries
+PRODUCT_COPY_FILES += \
+    $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
